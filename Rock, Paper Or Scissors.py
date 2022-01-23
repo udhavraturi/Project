536a595dd511af5b30ae375366281851b746a59a
udhavraturi <64944092+udhavraturi@users.noreply.github.com>
2022-01-23 11:42:32 +0530

    Update Rock, Paper Or Scissors.py

--- Rock, Paper Or Scissors.py
+++ Rock, Paper Or Scissors.py
@@ -1,47 +1,99 @@
 import random
-def gameWin(comp, you):
-    if you==comp:
-        return None
-    elif comp=='r':
-        if you=='p':
-            return "win"
-        elif you=='s':
-            return 'lose'
-    elif comp=='p':
-        if you=='s':
-            return "win"
-        elif you=='r':
-            return 'lose'
-    elif comp=='s':
-        if you=='r':
-            return "win"
-        elif you=='p':
-            return 'lose'
 
-        
-comp=print("Computers's turn: Rock (r),Paper (p) Or Scissor (s)? ")
-randNo= random.randint(1, 3)
-if randNo==1:
-    comp='r'
-elif randNo==2:
-    comp='p'
-else:
-    comp='s'
-print(randNo)
+choice=["Rock","Paper","Scissor"]
+
+while True:
+
+        print("Rock Paper scissor Game:")
+
+        youwin,computerwin=0,0
+
+        for i in range(1,6):
+
+                print("Round",i,"Start:")
+
+                print("Please select any one option:")
+
+                print("1.Rock","2.Paper","3.Scissor",sep="\n")
+
+                Yourchoice=int(input())
+
+                if Yourchoice==1:
+
+                        print("You select Rock")
+
+                        Yourchoice="Rock"
+
+                elif Yourchoice==2:
+
+                        print("You select paper")
+
+                        Yourchoice="Paper"
+
+                elif Yourchoice==3:
+
+                        print("You select scissor")
+
+                        Yourchoice="Scissor"
+
+                else:
+
+                        print("Invalid Choice")
+
+                        break
+
+                Computerchoice=random.choice(choice)
+
+                print("Computer select:",Computerchoice)
 
 
-you=input("Player's turn: Rock (r),Paper (p) Or Scissor (s)? ")
 
-a=gameWin(comp, you)
+                if Yourchoice==Computerchoice:
 
-print(f'computer choose {comp}')
-print(f'you choose {you}')
+                        youwin+=1
 
-if a==None:
-    print('Game is over')
-elif a:
-    print('you win')
-else:
-    print('you lose')
+                        computerwin+=1
 
- 
+                        print("This Round is Draw:")
+
+                elif (Yourchoice=="Paper" and Computerchoice=="Rock") or (Yourchoice=="Rock" and Computerchoice=="Scissor") or(Yourchoice=="Scissor" and Computerchoice=="Paper"):
+
+                        youwin+=1
+
+                        print("You win this Round")
+
+                else:
+
+                        computerwin+=1
+
+                        print("Computer win this Round")
+
+
+
+        if youwin>computerwin:
+
+                print("You Win the game:")
+
+                print("Score is:","Your score:",youwin,"Computer score:",computerwin,sep=" ")
+
+        elif computerwin>youwin:
+
+                print("You Lose the game. Computer win the game:")
+
+                print("Score is:","Your score:",youwin,"Computer score:",computerwin,sep=" ")
+
+        else:
+
+                print("Match Draw")
+
+                print("Score is:","Your score:",youwin,",Computer score:",computerwin,sep=" ")
+
+        user_choice=input("Want to Play again?(Yes/Exit)")
+
+        if user_choice=='yes' or user_choice=='Yes' or user_choice=='YES':
+
+                continue
+
+        else:
+
+                break
